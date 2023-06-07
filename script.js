db62b047ae3194c72f649d2f87df1f626e00bf4e
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2023-06-07 20:59:58 +0100

    Fix - Trail now works

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var cursorTrail;
 var lastActivityTime = new Date().getTime();
 var inactiveTime = 0;
 var trailDuration = 5000; // 5 seconds
+var trailPoints = []; // Array to store cursor positions for trail
 
 // Wait for DOM content to load
 document.addEventListener("DOMContentLoaded", function () {
@@ -26,6 +27,33 @@ function updateCursorTrail(event) {
   // Update cursor trail position
   cursorTrail.style.top = y + "px";
   cursorTrail.style.left = x + "px";
+
+  // Add current position to trailPoints array
+  trailPoints.push({ x: x, y: y });
+
+  // Update trail points positions
+  for (var i = 0; i < trailPoints.length; i++) {
+    var point = trailPoints[i];
+    var trailPoint = document.getElementById("trail-point-" + i);
+
+    if (!trailPoint) {
+      // Create new trail point element if it doesn't exist
+      trailPoint = document.createElement("div");
+      trailPoint.id = "trail-point-" + i;
+      trailPoint.style.position = "fixed";
+      trailPoint.style.width = "20px";
+      trailPoint.style.height = "20px";
+      trailPoint.style.borderRadius = "100%";
+      trailPoint.style.backgroundColor = "#000";
+      trailPoint.style.pointerEvents = "none";
+      trailPoint.style.zIndex = "9998"; // Ensure trail points are below the cursor trail
+      document.body.appendChild(trailPoint);
+    }
+
+    // Update trail point position
+    trailPoint.style.top = point.y + "px";
+    trailPoint.style.left = point.x + "px";
+  }
 }
 
 // Update cursor trail color based on inactivity
@@ -61,49 +89,6 @@ function getColorFromHeatmap(time) {
 
   // Return the RGB color as CSS value
   return "rgb(" + rgb.join(",") + ")";
-}
-
-// HSV to RGB color conversion
-function hsvToRgb(h, s, v) {
-  var r, g, b;
-  var i = Math.floor(h * 6);
-  var f = h * 6 - i;
-  var p = v * (1 - s);
-  var q = v * (1 - f * s);
-  var t = v * (1 - (1 - f) * s);
-  switch (i % 6) {
-    case 0:
-      r = v;
-      g = t;
-      b = p;
-      break;
-    case 1:
-      r = q;
-      g = v;
-      b = p;
-      break;
-    case 2:
-      r = p;
-      g = v;
-      b = t;
-      break;
-    case 3:
-      r = p;
-      g = q;
-      b = v;
-      break;
-    case 4:
-      r = t;
-      g = p;
-      b = v;
-      break;
-    case 5:
-      r = v;
-      g = p;
-      b = q;
-      break;
-  }
-  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 
 // Add event listener for mousemove to update cursor trail position and color
